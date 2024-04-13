e3f63c4dc6d85e39d79f61156ae2a3a6c8387f65
toa.pita <toa.pita@gmail.com>
2024-04-12 23:17:05 -0600

    npx create-next-app

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Just proving I can do Next.js and AWS Amplify</h1>
+    </>
+  );
+}
